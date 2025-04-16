15a06ec27dfcbd0cbae7fbc6964702a424bc0a7a
Ravi Sharma <ravi2018cs115abesit@gmail.com>
2025-04-16 15:22:38 +0530

    Redux setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactDom from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import store from './redux/store.js'
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
