2591f4b0983db66d038d357e60967c18b897b7f4
Ravi Sharma <ravi2018cs115abesit@gmail.com>
2025-04-16 15:26:23 +0530

    dummy data added

---
+++ src/data.js
@@ -0,0 +1,58 @@
+import { original } from "@reduxjs/toolkit";
+
+export const products = [
+    {
+        id: 1,
+        name: "Milk",
+        weight: "500ml",
+        price: 28,
+        originalPrice: 30,
+        image: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=s…977-31ab-4d8e-abf3-d42e4a4b4632.jpg?ts=1706182142"
+
+    },
+    {
+        id: 2,
+        name: "Milk",
+        weight: "500ml",
+        price: 28,
+        originalPrice: 30,
+        image: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=s…977-31ab-4d8e-abf3-d42e4a4b4632.jpg?ts=1706182142"
+
+    },
+    {
+        id: 3,
+        name: "Milk",
+        weight: "500ml",
+        price: 28,
+        originalPrice: 30,
+        image: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=s…977-31ab-4d8e-abf3-d42e4a4b4632.jpg?ts=1706182142"
+
+    },
+    {
+        id: 4,
+        name: "Milk",
+        weight: "500ml",
+        price: 28,
+        originalPrice: 30,
+        image: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=s…977-31ab-4d8e-abf3-d42e4a4b4632.jpg?ts=1706182142"
+
+    },
+    {
+        id: 5,
+        name: "Milk",
+        weight: "500ml",
+        price: 28,
+        originalPrice: 30,
+        image: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=s…977-31ab-4d8e-abf3-d42e4a4b4632.jpg?ts=1706182142"
+
+    },
+    {
+        id: 6,
+        name: "Milk",
+        weight: "500ml",
+        price: 28,
+        originalPrice: 30,
+        image: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=s…977-31ab-4d8e-abf3-d42e4a4b4632.jpg?ts=1706182142"
+
+    }
+]
